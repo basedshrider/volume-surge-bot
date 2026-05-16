551450c8b5da1a2c6d1b038832d51b27d096e53b
basedshrider <drophunter8207@gmail.com>
2026-05-16 21:46:21 +0530

    Clean up: delete extra files for single-file bot

--- handlers/callbacks.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer("Action received")
